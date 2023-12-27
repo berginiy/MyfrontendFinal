183c038c9d0ace251ea43527d3f4a0b8e8b64ed2
Altyn <pirozokaltynbek@gmail.com>
2023-12-27 18:37:37 +0600

    get git clone

--- main/chatgpt/apps.py
+++
@@ -1,8 +0,0 @@
-# chatgpt/apps.py
-from django.apps import AppConfig
-
-
-class ChatGptConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chatgpt'
-
--- main/chatgpt/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ChatMasterView, StoryView,CustomUserList
-
-urlpatterns = [
-    path('game/', ChatMasterView.as_view(), name='game_master'),
-    path('story/', StoryView.as_view(), name='story'),
-    path('gameinfo/', CustomUserList.as_view(), name='list'),
-
-]
