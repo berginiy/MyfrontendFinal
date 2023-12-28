ba23ac26f4e3f6d5655d07b6b90f603e94293dc7
Amanbaev Bayastan <maintrot0525@gmail.com>
2023-12-28 07:13:00 +0600

    try something

--- vite-project/src/components/form/form.jsx
+++ vite-project/src/components/form/form.jsx
@@ -1,14 +1,43 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 export default function Form() {
+  const [gameUserName, setGameUserName] = useState()
+  const [gameUserRole, setGameUserRole] = useState()
+
+  function setInputGameUserName(event) {
+    setGameUserName(event.target.value)
+  }
+
+  function setInputGameUserRole(event) {
+    setGameUserRole(event.target.value)
+  }
+
+  function sendForm(event) {
+    event.preventDefault();
+
+    axios.post(`http://127.0.0.1:8000/api/v1/chatgpt/story`, {
+      name: gameUserName,
+      role: gameUserRole,
+      health: 100,
+      hunger: 100,
+      thirst: 100,
+      user: 25
+    })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
   return(
     <div className="body_form">
-      <form action="" method="post">
+      <form onSubmit={sendForm} action="" method="post">
         <h2>Story</h2>
-        <label htmlFor="lore">lore:</label>
-        <input type="text" name='lore' placeholder="write your personal lore for the game" />
+        <label htmlFor="name">name:</label>
+        <input onChange={setInputGameUserName} type="text" name='name' placeholder="write your personal name for the game" />
         <label htmlFor="role">role:</label>
-        <input type="text" name='role' placeholder="write your personal role for the game" />
-        <label htmlFor="hp">HP:</label>
-        <input type='number' name='hp' placeholder="write value of your HP" />
+        <input onChange={setInputGameUserRole} type="text" name='role' placeholder="write your personal role for the game" />
         <button className='form_blue' type="button">HELL YEAH</button>
       </form>
     </div>
--- vite-project/src/components/mains/MainForMain.jsx
+++ vite-project/src/components/mains/MainForMain.jsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../App';
+import { useContext } from 'react';
 
 export default function MainForMain() {
+  const [user, setUser] = useContext(UserContext)
   return(
     <main className="main_container">
       <section className='bird'>
+        <div className='section_left'>
         <div className="section_text">
           <h2>D&D game with ChatGPT</h2>
           <p>this is text about such a good our game that you want to give all your money and maybe give 100 point you know</p>
@@ -11,19 +15,16 @@ export default function MainForMain() {
         <div className="btn">
           <Link className='link' to='/game'>Start Game</Link>
         </div>
-        <div className="section">
-          <div className="pip">
-            <div className="about">
-              <div className="about_item">
-                <div className="about_img">
-                  <div className='game_div'></div>
-                  <div className='game_div'></div>
-                  <div className='game_div'></div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
+        {!user ? (
+        <>
+        </>) : (
+          <div className="section_right">
+            <div className='game_div'></div>
+            <div className='game_div'></div>
+            <div className='game_div'></div>
+          </div>
+        )}
       </section>
     </main>
   )
